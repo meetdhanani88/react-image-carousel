82bdea30d65728779688c32485c87304962631de
meetdhanani88 <meetdhanani87@gmaiil.com>
2025-03-11 14:30:36 +0530

    Removed Common JS Build

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,32 @@
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import postcss from 'rollup-plugin-postcss';
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
 
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === "production";
 
 export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: !production,
-    },
-    {
-    file: 'dist/index.esm.js',
-    format: 'esm',
-      sourcemap: !production,
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.esm.js",
+    format: "esm",
+    sourcemap: !production,
   },
-  ],
-  external: ['react', 'react-dom'],
+  external: ["react", "react-dom"],
   watch: {
-    include: 'src/**',
-    exclude: 'node_modules/**',
+    include: "src/**",
+    exclude: "node_modules/**",
     clearScreen: false,
   },
   plugins: [
     resolve(),
-    commonjs(),
     typescript({
-      tsconfig: './tsconfig.json',
+      tsconfig: "./tsconfig.json",
       sourceMap: !production,
       inlineSources: !production,
     }),
     postcss({
       modules: false,
-      use: ['sass'],
+      use: ["sass"],
       sourceMap: !production,
       inject: true,
     }),
